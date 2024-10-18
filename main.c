fc12615a3974afff1008c835f4dee996919c0253
Santiago <santiramos.cr@gmail.com>
2024-10-17 19:24:11 -0600

    primero

--- main.c
+++ main.c
@@ -2,8 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+int arreglo_frecuencias [256]; 
+
+
+void mostrarByteEnBinario(unsigned char byte){
+    for (int i = 7; i >= 0; i--){
+        printf("%d", (byte >> i) & 1);
+    }
+}
 int main(){
-    const char* nombre_archivo = "archivo.txt";
+    const char* nombre_archivo = "archivoprueba.txt";
     FILE *archivo = fopen(nombre_archivo, "rb");
 
     if(archivo == NULL){
@@ -12,8 +21,41 @@ int main(){
     }
 
     fseek(archivo, 0, SEEK_END);
-    long tamanArchivo = ftell(archivo);
+    long tamanoArchivo = ftell(archivo);
     rewind(archivo);
+
+    unsigned char *buffer = (unsigned char*) malloc(sizeof(unsigned char) * tamanoArchivo);
+    if (buffer == NULL) {
+        printf("No se pudo asignar memoria para leer el archivo\n");
+        fclose(archivo);
+        return 1;
+    }
+
+    size_t bytesLeidos = fread(buffer, sizeof(unsigned char), tamanoArchivo, archivo);
+    if (bytesLeidos != tamanoArchivo) {
+        printf("Error al leer el archivo\n");
+        free(buffer);
+        fclose(archivo);
+        return 1;
+    }
+
+    // Mostrar los bytes leídos (en formato hexadecimal)
+    for (long i = 0; i < tamanoArchivo; i++) {
+        mostrarByteEnBinario(buffer[i]);
+        printf(" ");
+        arreglo_frecuencias[buffer[i]]++;
+    }
+    printf("\n");
+
+    //imprimir el arreglo frecuencias
+    for (int i =0; i<255;i++){
+        printf("Arreglo[%d] = %d \n",i,arreglo_frecuencias[i]);
+    }
+    // Liberar la memoria y cerrar el archivo
+    free(buffer);
+    fclose(archivo);
+
+    return 0;
 
     
 }
